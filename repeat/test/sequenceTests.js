7c660cffe64c9ea2f9ab482c26876db2379b37e2
Duncan Awerbuck <dawerbuck@deloitte.ie>
2015-12-31 12:28:22 +0000

    Initial Web Site Project

---
+++ repeat/test/sequenceTests.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="lib/chai.js" />
+chai.should();
+
+describe("My first wallaby test", function () {
+    it("should work", function () {
+        true.should.equal(true);
+    });
+});
